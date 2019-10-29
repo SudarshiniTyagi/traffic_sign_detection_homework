4003675e65cbc59d867dcf2a8b3bb0e835678054
Tyagi <st3688@dlrlpdcdap004.nyumc.org>
2019-10-28 20:08:34 -0400

    Evaluates on ensemble

--- evaluate.py
+++ evaluate.py
@@ -10,24 +10,57 @@ import torch.nn.functional as F
 import torchvision.datasets as datasets
 
 from data import initialize_data # data.py in the same folder
-from model import Net
+from resnet_model import Net
+from google_net import Net as Net2
+from resnet_model_2 import Net as Net3
+from stn import Net as Net4
 
 parser = argparse.ArgumentParser(description='PyTorch GTSRB evaluation script')
 parser.add_argument('--data', type=str, default='data', metavar='D',
                     help="folder where data is located. train_data.zip and test_data.zip need to be found in the folder")
-parser.add_argument('--model', type=str, metavar='M',
+parser.add_argument('--model1', type=str, metavar='M',
+                    help="the model file to be evaluated. Usually it is of the form model_X.pth")
+parser.add_argument('--model2', type=str, metavar='M',
+                    help="the model file to be evaluated. Usually it is of the form model_X.pth")
+parser.add_argument('--model3', type=str, metavar='M',
+                    help="the model file to be evaluated. Usually it is of the form model_X.pth")
+parser.add_argument('--model4', type=str, metavar='M',
                     help="the model file to be evaluated. Usually it is of the form model_X.pth")
 parser.add_argument('--outfile', type=str, default='gtsrb_kaggle.csv', metavar='D',
                     help="name of the output csv file")
 
 args = parser.parse_args()
 
-state_dict = torch.load(args.model)
-model = Net()
-model.load_state_dict(state_dict)
-model.eval()
 
-from data import data_transforms
+
+state_dict_1 = torch.load(args.model1)
+model_1 = Net()
+model_1.load_state_dict(state_dict_1)
+model_1.eval()
+model_1.to("cuda:0")
+
+state_dict_2 = torch.load(args.model2)
+model_2 = Net2()
+model_2.load_state_dict(state_dict_2)
+model_2.eval()
+model_2.to("cuda:0")
+
+state_dict_3 = torch.load(args.model3)
+model_3 = Net3()
+model_3.load_state_dict(state_dict_3)
+model_3.eval()
+model_3.to("cuda:0")
+
+state_dict_4 = torch.load(args.model4)
+model_4 = Net4()
+model_4.load_state_dict(state_dict_4)
+model_4.eval()
+model_4.to("cuda:0")
+
+
+
+
+from data import data_transforms, data_transforms2
 
 test_dir = args.data + '/test_images'
 
@@ -43,9 +76,17 @@ output_file.write("Filename,ClassId\n")
 for f in tqdm(os.listdir(test_dir)):
     if 'ppm' in f:
         data = data_transforms(pil_loader(test_dir + '/' + f))
+        data2 = data_transforms2(pil_loader(test_dir + '/' + f))
         data = data.view(1, data.size(0), data.size(1), data.size(2))
-        data = Variable(data, volatile=True)
-        output = model(data)
+        data = Variable(data, volatile=True).to("cuda:0")
+        data2 = data2.view(1, data2.size(0), data2.size(1), data2.size(2))
+        data2 = Variable(data2, volatile=True).to("cuda:0")
+        
+        output_1 = model_1(data)
+        output_2 = model_2(data)
+        output_3 = model_3(data)
+        output_4 = model_4(data2)
+        output = (output_1+output_2+output_3+output_4)/4
         pred = output.data.max(1, keepdim=True)[1]
 
         file_id = f[0:5]
